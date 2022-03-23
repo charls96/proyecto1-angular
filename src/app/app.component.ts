dc6df50b183a77c3c1c5c79e66a50aff53d8b9a0
Carlos Daniel Lopez Perez <cloppere@emeal.nttdata.com>
2022-03-23 10:43:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto1';
+  title:string = 'Contador';
+  num: number = 0;
 }
