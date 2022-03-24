c3d69f2827170ee6d369a927d003c4baded84488
Carlos Daniel Lopez Perez <cloppere@emeal.nttdata.com>
2022-03-24 11:33:35 +0100

    actividad formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -42,6 +42,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
--- src/app/c-seccion2/c-seccion2.component.ts
+++ src/app/c-seccion2/c-seccion2.component.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-c-seccion2',
   templateUrl: './c-seccion2.component.html',
-  styleUrls: ['./c-seccion2.component.css']
+  styleUrls: ['./c-seccion2.component.css'],
 })
 export class CSeccion2Component implements OnInit {
+  ciudad: string | undefined;
+  poblacion: string | undefined;
 
-  ciudad: string|undefined;
-  poblacion: string|undefined;
+  formComentario:FormGroup = new FormGroup( {
+    usuario: new FormControl('', Validators.required),
+      comentario: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+  });
 
-  constructor(private route: ActivatedRoute, private router: Router) { };
-
-  ngOnInit(): void {
-    this.route.params.subscribe( params => {
-      if (params['ciudad'] != null){
-        this.ciudad = params['ciudad']
-      }
-      if (params['poblacion'] != null){
-        this.poblacion = params['poblacion']
-      }
-    })
-
+  public onSubmit(formValue: any) {
+    console.log(`El usuario '${formValue.usuario}' ha excrito el comentario: '${formValue.comentario}' `);
   }
 
+  get formValues() {
+    return this.formComentario.controls;
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      if (params['ciudad'] != null) {
+        this.ciudad = params['ciudad'];
+      }
+      if (params['poblacion'] != null) {
+        this.poblacion = params['poblacion'];
+      }
+    });
+  }
 }
